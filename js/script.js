bb13f0700b7528ee4c8aaadcbfa12947b277d4d1
Julian Rodriguez <92952502+juleanrod@users.noreply.github.com>
2022-09-16 14:59:10 -0700

    Add changes to labels when switching from dark/light modes

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const button2 = document.querySelector('.dev');
 button2.addEventListener('click', changeToDev);
 
 const section1 = document.querySelector('.section1');
+
 let mode = "site";
 function scrollToTop(){
     window.scrollTo({top: 0, behavior: 'smooth'});
@@ -106,7 +107,9 @@ function nitemode(){
         root.classList.add('light');
         root.classList.remove('dark');
         nitemodeBtn.innerHTML = '<svg width="18px" height="18px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="var(--navbar-icon-color)"><g><path fill="none" d="M0 0h24v24H0z"/><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z"/></g></svg>'
+        nitemodeBtn.setAttribute("title", "Light mode");
         nite = false;
+
     }
 
 }
